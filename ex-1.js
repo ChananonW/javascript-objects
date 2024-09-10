c7ac25854e3ed4a0beb20dda9ff937f238f49337
FrankCW <wongasa999@gmail.com>
2024-09-10 14:55:45 +0700

    Objects Assignment ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,14 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const FoodOrder = {
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentype: "Cash",
+    totalPrice: 4500,
+    restaurantName: "MK"
+};
+
+FoodOrder.totalPrice = 5000;
+FoodOrder.paymentype = "Credit Card";
+console.log(FoodOrder)
